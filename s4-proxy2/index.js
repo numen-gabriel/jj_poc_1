8e1f3b47c4b51901e98566c7cc2aa5cdd77511c1
ecassaro1 <ericcassaro@yahoo.com>
2024-01-17 13:34:22 -0300

    cors

--- s4-proxy2/index.js
+++ s4-proxy2/index.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
+var cors = require('cors');
+
 app.use(express.json()) 
+app.use(cors());
+
 const multer = require('multer');
 const upload = multer();
 
